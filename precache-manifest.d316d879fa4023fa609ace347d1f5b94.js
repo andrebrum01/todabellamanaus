c5ca47f85fb9e370b1cb8d2f65e2273141a48be4
andrebrum01 <andrezinhobrun@gmail.com>
2020-06-30 17:45:25 -0400

    Updates

---
+++ precache-manifest.d316d879fa4023fa609ace347d1f5b94.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9fbf364b21e6faeec16a5ed08e3504b",
+    "url": "/todabellamanaus/index.html"
+  },
+  {
+    "revision": "2418135aad30076fbe1e",
+    "url": "/todabellamanaus/static/css/4.e5c64934.chunk.css"
+  },
+  {
+    "revision": "9fc5a93ba8b7f5c68de1",
+    "url": "/todabellamanaus/static/css/main.2ccb055d.chunk.css"
+  },
+  {
+    "revision": "5db33ff5a12f9e1c600a",
+    "url": "/todabellamanaus/static/js/2.c2f0790e.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/todabellamanaus/static/js/2.c2f0790e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d267c3c01e89a7e9f33",
+    "url": "/todabellamanaus/static/js/3.3fffc3cf.chunk.js"
+  },
+  {
+    "revision": "2418135aad30076fbe1e",
+    "url": "/todabellamanaus/static/js/4.5f4d4d3b.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/todabellamanaus/static/js/4.5f4d4d3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bd1f8bf0643f1c36bfc",
+    "url": "/todabellamanaus/static/js/5.1a1c2388.chunk.js"
+  },
+  {
+    "revision": "a44495f7da354dd8f8c1",
+    "url": "/todabellamanaus/static/js/6.a1f7e1a9.chunk.js"
+  },
+  {
+    "revision": "f9ab9e97c936381b2cbf",
+    "url": "/todabellamanaus/static/js/7.5c2600b8.chunk.js"
+  },
+  {
+    "revision": "9fc5a93ba8b7f5c68de1",
+    "url": "/todabellamanaus/static/js/main.1f865827.chunk.js"
+  },
+  {
+    "revision": "f30553b1e42bc3d0a32f",
+    "url": "/todabellamanaus/static/js/runtime-main.5bc6b609.js"
+  },
+  {
+    "revision": "38393b0e3582c4250e56aba963a05d07",
+    "url": "/todabellamanaus/static/media/acneica.38393b0e.jpg"
+  },
+  {
+    "revision": "dcada123713762af2de051f332651e07",
+    "url": "/todabellamanaus/static/media/add.dcada123.svg"
+  },
+  {
+    "revision": "17119dbf01930a9c3c0e5ab1ce31e7e8",
+    "url": "/todabellamanaus/static/media/arrow.17119dbf.svg"
+  },
+  {
+    "revision": "527bea50a0f03a5c3f20fa59cc767c0a",
+    "url": "/todabellamanaus/static/media/arrowBlack.527bea50.svg"
+  },
+  {
+    "revision": "3298f58606e0eb3bce4545eee2af1117",
+    "url": "/todabellamanaus/static/media/avulsos.3298f586.jpg"
+  },
+  {
+    "revision": "bea64f3dd75a45e457bf47bc13564cfe",
+    "url": "/todabellamanaus/static/media/carrinho.bea64f3d.svg"
+  },
+  {
+    "revision": "abe54a078bf0a2d36066052e20f3ae67",
+    "url": "/todabellamanaus/static/media/cicatrizdeacne.abe54a07.jpg"
+  },
+  {
+    "revision": "d467865ebc80a097f7c39df1aeec6b7c",
+    "url": "/todabellamanaus/static/media/corporal.d467865e.png"
+  },
+  {
+    "revision": "35b3b83985f285d1e8e31a44440132a4",
+    "url": "/todabellamanaus/static/media/depilacao.35b3b839.jpg"
+  },
+  {
+    "revision": "b189b88808247cd190d6d40259625df8",
+    "url": "/todabellamanaus/static/media/detox.b189b888.jpg"
+  },
+  {
+    "revision": "5b8f1f325c04e9c7df62a9026379b66d",
+    "url": "/todabellamanaus/static/media/drenagem.5b8f1f32.jpg"
+  },
+  {
+    "revision": "5afdfd91b21302777cd0e851a4d87aea",
+    "url": "/todabellamanaus/static/media/efeitoLipo.5afdfd91.jpg"
+  },
+  {
+    "revision": "0336b03daef511cba4ade84d88c44e83",
+    "url": "/todabellamanaus/static/media/empinabumbum.0336b03d.jpg"
+  },
+  {
+    "revision": "5b3f2a6167dda8a22885f0de13842c88",
+    "url": "/todabellamanaus/static/media/faciais.5b3f2a61.jpg"
+  },
+  {
+    "revision": "07512793350b2ed9241b2d8ab94d266c",
+    "url": "/todabellamanaus/static/media/facial.07512793.jpg"
+  },
+  {
+    "revision": "b5d6adba58a69a858061df568a23d1dd",
+    "url": "/todabellamanaus/static/media/flacidez.b5d6adba.jpg"
+  },
+  {
+    "revision": "a6e14c68d401e2c35483a7bf065a038c",
+    "url": "/todabellamanaus/static/media/gordura.a6e14c68.jpg"
+  },
+  {
+    "revision": "fdc5096f94e8e2e61177494d41304e1f",
+    "url": "/todabellamanaus/static/media/inta.fdc5096f.svg"
+  },
+  {
+    "revision": "479e70f7d61e72fcbc9041c4f25399f9",
+    "url": "/todabellamanaus/static/media/logo.479e70f7.svg"
+  },
+  {
+    "revision": "f1608cf5b29234cc0f59a87201bb8df2",
+    "url": "/todabellamanaus/static/media/manchas.f1608cf5.jpg"
+  },
+  {
+    "revision": "8fd683dc9b332ef7d2b468e994abadf1",
+    "url": "/todabellamanaus/static/media/massagem.8fd683dc.jpg"
+  },
+  {
+    "revision": "1e070a6818ba8703d2a28f720374169a",
+    "url": "/todabellamanaus/static/media/newface.1e070a68.jpg"
+  },
+  {
+    "revision": "88470876775a9f73e4f2488cc9800581",
+    "url": "/todabellamanaus/static/media/posparto.88470876.jpg"
+  },
+  {
+    "revision": "19a94fedf94fe8812ced423a050ecb72",
+    "url": "/todabellamanaus/static/media/semflacidez.19a94fed.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/todabellamanaus/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/todabellamanaus/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/todabellamanaus/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/todabellamanaus/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "f353370f6cb30e0a792953c27e839c98",
+    "url": "/todabellamanaus/static/media/titleCategoria.f353370f.svg"
+  },
+  {
+    "revision": "0ffc6719fdfe2f9e146e4b502c65673d",
+    "url": "/todabellamanaus/static/media/titleObjetivo.0ffc6719.svg"
+  },
+  {
+    "revision": "82943c89ccc672d194ac264ec0181376",
+    "url": "/todabellamanaus/static/media/todabella.82943c89.jpg"
+  }
+]);
